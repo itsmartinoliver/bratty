ba98e83082ff50e0b541ace87c93cd8459d6e9fd
Brady Kloek <bradywkloek@gmail.com>
2025-12-30 10:44:27 -0600

    Customizable configs

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import json
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-class QuestionBot(commands.Bot):
+class BrattyBot(commands.Bot):
     def __init__(self, intents):
         super().__init__(command_prefix="!", intents=intents)
+
+        self.config_path = "config.json"
+        if os.path.exists(self.config_path):
+            with open(self.config_path) as f:
+                self.config = json.load(f)
+        else:
+            self.config = {
+                "Global": {
+                    "timezone": "America/Chicago"
+                }
+            }
+            self.save_config()
 
     async def setup_hook(self):
         await self.load_extension("cogs.questions")
         await self.load_extension("cogs.show_and_tell")
         await self.load_extension("cogs.respond")
 
-bot = QuestionBot(intents)
+    def save_config(self):
+        with open(self.config_path, "w") as f:
+            json.dump(self.config, f, indent=4)
+            
+bot = BrattyBot(intents)
 
 @bot.command()
 @commands.has_permissions(administrator=True)
@@ -27,5 +44,21 @@ async def reload(ctx, extension):
         await ctx.send(f"Successfully reloaded `{extension}`")
     except Exception as e:
         await ctx.send(f"Failed to reload: `{e}`")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def config(ctx, cog, field, value):
+    try:
+        bot.config[cog][field] = value
+        await ctx.send(f"Updated config: {cog}/{field} = {value}")
+        bot.save_config()
+    except Exception as e:
+        await ctx.send(f"Failed to update config: `{e}`")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def view_config(ctx):
+    print(f"Command triggered! Memory ID: {id(bot)}")
+    await ctx.send("```\n"+json.dumps(bot.config, indent=4)+"\n```")
 
 bot.run(TOKEN)
