cdac07bc7e903a7bdd73e50a21b0c7a4ba1b527a
Brady Kloek <bradywkloek@gmail.com>
2026-01-03 20:53:10 -0600

    Quips cog

--- cogs/quip.py
+++ cogs/quip.py
@@ -4,7 +4,8 @@ from zoneinfo import ZoneInfo
 import os
 import random
 
-history_path = "quips/history.txt"
+history_path = "quip_data/history.txt"
+quip_path = "quip_data/quips.txt"
 
 os.makedirs(os.path.dirname(history_path), exist_ok=True)
 
@@ -13,6 +14,9 @@ if not os.path.exists(history_path):
     with open(history_path, "w") as f:
         pass
 
+def clear_history():
+    with open(history_path, "r+") as history_f:
+        history_f.truncate(0)
 
 class Quip(commands.Cog):
     def __init__(self, bot):
@@ -22,28 +26,89 @@ class Quip(commands.Cog):
             bot.config["quip"] = {
                 "daily_probability": "0.5",
                 "start_hour": "9",
-                "end_hour": "17",
+                "end_hour": "16",
                 "start_minute": "0",
                 "end_minute": "59",
+                "max_entries": "50",
                 "channels": []
             }
 
-        self.debug = bool(bot.config["quip"]["debug"])
         self.daily_probability = float(bot.config["quip"]["daily_probability"])
-        self.start_time = int(bot.config["quip"]["start_time"])
-        self.end_time = int(bot.config["quip"]["end_time"])
+        self.start_hour = int(bot.config["quip"]["start_hour"])
+        self.end_hour = int(bot.config["quip"]["end_hour"])
+        self.start_minute = int(bot.config["quip"]["start_minute"])
+        self.end_minute = int(bot.config["quip"]["end_minute"])
+        self.max_entries = int(bot.config["quip"]["max_entries"])
         self.channels = bot.config["quip"]["channels"]
+        self.timezone = ZoneInfo(bot.config["global"]["timezone"])
+
+        self.plan_quip.change_interval(time=datetime.time(hour=0, tzinfo=self.timezone))
+        self.plan_quip.start()
 
         bot.save_config()
 
-        @tasks.loop(time=datetime.time(hour=0)) # Loop timing is changed in the constructor
-        async def plan_quip(self):
-            rng = random.random()
-            if random <= self.daily_probability:
-                
-            else:
-                print("Quip not planned")
-            await self.post_thread(question_choice)
+    def cog_unload(self):
+        self.plan_quip.cancel()
+        if self.send_quip.is_running:
+            self.send_quip.cancel()
+
+    @tasks.loop(time=datetime.time(hour=0)) # Loop timing timezone is set by constructor
+    async def plan_quip(self):
+        rng = random.random()
+        if rng <= self.daily_probability:
+            quip_hour = random.randint(self.start_hour, self.end_hour)
+            quip_minute = random.randint(self.start_minute, self.end_minute)
+
+            if self.send_quip.is_running:
+                self.send_quip.stop()
+            self.send_quip.change_interval(time=datetime.time(hour=quip_hour, minute=quip_minute, tzinfo=self.timezone))
+            self.send_quip.start()
+            print(f"Quip planned for {quip_hour}:{quip_minute:02d}", flush=True)
+        else:
+            self.send_quip.stop()
+            print("Quip not planned", flush=True)
+
+
+    @tasks.loop(time=datetime.time(hour=0)) # Loop timing will be changed by plan_quip
+    async def send_quip(self):
+        channelID = int(random.choice(self.channels))
+        channel = self.bot.get_channel(channelID)
+        quip = self.choose_quip()
+        await channel.send(quip)
+        
+    def choose_quip(self):
+        with open(quip_path, "r") as quip_fp:
+            quips = quip_fp.read().splitlines()
+        with open(history_path, "r") as history_fp:
+            history = history_fp.read().splitlines()
+
+        quip_choice = random.choice(quips)
+        while quip_choice in history:
+            quip_choice = random.choice(quips)
+
+        self.add_to_history(quip_choice)
+        return quip_choice
+        
+    def add_to_history(self, new_entry):
+        with open(history_path, "r") as history_f:
+            lines = history_f.read().splitlines()
+            while len(lines) >= self.max_entries:
+                lines.pop(0)    # remove the top line, which is the oldest entry
+            lines.append(new_entry)    # final entry did not previously have a new line, must add it now
+        
+        with open(history_path, "w") as history_f:
+            history_f.write("\n".join(lines))
+
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def test_plan_quip(self, ctx):
+        await self.plan_quip()
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def test_send_quip(self, ctx):
+        await self.send_quip()
 
 async def setup(bot):
     await bot.add_cog(Quip(bot))
