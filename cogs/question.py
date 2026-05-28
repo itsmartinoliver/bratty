604415f5c062480443c9d259aceb4458fbb1931b
Brady Kloek <bradywkloek@gmail.com>
2026-05-28 16:41:53 -0500

    Updated Question Logic for repeats

--- cogs/question.py
+++ cogs/question.py
@@ -7,6 +7,7 @@ import random
 
 history_path = "question_data/history.txt"
 questions_path = "question_data/questions.tsv"
+date_format = "%m/%d/%Y"
 
 os.makedirs(os.path.dirname(history_path), exist_ok=True)
 
@@ -26,14 +27,12 @@ class Question(commands.Cog):
                 "role": "0",
                 "hour": "8",
                 "minute": "0",
-                "max_entries": "50"
             }
 
         self.channel = int(bot.config["question"]["channel"])
         self.role = int(bot.config["question"]["role"])
         self.hour = int(bot.config["question"]["hour"])
         self.minute = int(bot.config["question"]["minute"])
-        self.max_entries = int(bot.config["question"]["max_entries"])
         self.timezone = ZoneInfo(bot.config["global"]["timezone"])
 
         self.daily_question.change_interval(time=datetime.time(hour=self.hour, minute=self.minute, tzinfo=self.timezone))
@@ -47,7 +46,7 @@ class Question(commands.Cog):
     
     @tasks.loop(time=datetime.time(hour=0)) # Loop timing is changed in the constructor
     async def daily_question(self):
-        question_choice = self.choose_question()
+        question_choice = await self.choose_question()
         await self.post_thread(question_choice)
 
     async def post_thread(self, content):
@@ -56,7 +55,7 @@ class Question(commands.Cog):
         if len(content) > 100:
             thread_content = thread_content[:97] + "..."
         if channel:
-            date = datetime.datetime.now(self.timezone).strftime("%m/%d/%Y")
+            date = datetime.datetime.now(self.timezone).strftime(date_format)
             message = await channel.send(f"## Question of the Day: {date}")
             thread = await message.create_thread(name=f"{thread_content}", auto_archive_duration = 60*24)
             await thread.send(f"<@&{self.role}>: {content}")
@@ -73,60 +72,55 @@ class Question(commands.Cog):
         with open(history_path, "r+") as history_f:
             history_f.truncate(0)
 
-    def make_question_lists(self):
+    async def make_question_list(self):
         """
         Returns parallel lists:
             questions: contains each question as a string
             weights: contains the weightings of each question as a float
         """
-        questions = []
-        weights = []
+        valid_questions = []
+        time_now = datetime.datetime.now(self.timezone)
+        hist_dict = self.make_hist_dict()
+
         with open(questions_path, "r") as questions_f:
             for line in questions_f.readlines()[1:]:  # skip header
-                split_line = line.split("\t")
-                questions.append(split_line[0].strip()) # strip() is just for safety, shouldn't normally have any effect
-                weights.append(float(split_line[1]))
-        return questions, weights
+                split_line = line.strip().split("\t")
+                question = split_line[0]
+                min_days = int(split_line[1]) if len(split_line) > 1 else -1
+                # min_days == -1 indicates no repeats are allowed
+                if self.is_valid(question, min_days, hist_dict, time_now):
+                    valid_questions.append(question)
+        await self.bot.debug(str(len(valid_questions)) + " valid questions remaining")
+        return valid_questions
 
-    def remove_recent(self, questions, weights):
-        """
-        Produces new parallel lists (questions, weights) containing only the questions that
-        do NOT appear in history.txt
-        """
-        if(len(questions) != len(weights)):
-            raise RuntimeError("Questions and Weights do not have the same length")
+    def make_hist_dict(self):
+        hist_dict = {}
+        with open(history_path, "r") as history_f:
+            for line in history_f.readlines():
+                if line == "\n":
+                    break
+                split_line = line.strip().split("\t")
+                hist_dict[split_line[0]] = datetime.datetime.strptime(split_line[1], date_format).replace(tzinfo=self.timezone)
+        return hist_dict
 
-        history_entries = []
-        with open(history_path, "r") as history:
-            for line in history.readlines():
-                history_entries.append(line.strip())
-        new_questions = []
-        new_weights = []
+    def is_valid(self, question, min_days, hist_dict, time_now):
+        if question not in hist_dict:
+            return True
+        if min_days == -1:
+            return False
+        time_asked = hist_dict[question]
+        time_since = time_now - time_asked
+        return time_since >= datetime.timedelta(days=float(min_days))
 
-        for i in range(len(questions)):
-            if questions[i] not in history_entries:
-                new_questions.append(questions[i])
-                new_weights.append(weights[i])
-
-        return new_questions, new_weights
-
-    def choose_question(self):
-        questions, weights = self.make_question_lists()
-        questions, weights = self.remove_recent(questions, weights)
-        # Using random.choices to utilize weights. This returns a list, so we need to index the element
-        question_choice = random.choices(questions, weights=weights)[0]
+    async def choose_question(self):
+        questions = await self.make_question_list()
+        question_choice = random.choice(questions)
         self.add_to_history(question_choice)
         return question_choice
 
     def add_to_history(self, new_entry):
-        with open(history_path, "r") as history_f:
-            lines = history_f.read().splitlines()
-            while len(lines) >= self.max_entries:
-                lines.pop(0)    # remove the top line, which is the oldest entry
-            lines.append(new_entry)    # final entry did not previously have a new line, must add it now
-        
-        with open(history_path, "w") as history_f:
-            history_f.write("\n".join(lines))
+        with open(history_path, "a") as history_f:
+            history_f.write(new_entry + "\t" + datetime.datetime.now(self.timezone).strftime(date_format) + "\n")
 
 async def setup(bot):
     await bot.add_cog(Question(bot))
